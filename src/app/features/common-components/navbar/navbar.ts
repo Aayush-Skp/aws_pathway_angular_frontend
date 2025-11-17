f174f898dca601beeab60f6bf299ef792a58c5f6
aayushskp <krishnapanthi94@gmail.com>
2025-11-17 18:29:10 +0545

    added navbar

--- src/app/features/common-components/navbar/navbar.ts
+++ src/app/features/common-components/navbar/navbar.ts
@@ -10,44 +10,13 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './navbar.scss',
 })
 export class Navbar {
-  currentDate: string = 'बुधबार, फदौं २५, २०८२';
-  currentTime: string = '१४ : ०४ : ५४';
-  showSearch: boolean = false;
-  searchQuery: string = '';
+  isDropdownOpen = false;
 
-  menuItems = [
-    { name: 'मुहुण्ड', link: '/muhund' },
-    { name: 'मिडिया स्टोरी', link: '/media-story' },
-    { name: 'राजनीति', link: '/politics' },
-    { name: 'बजार अर्थतन्त्र', link: '/market-economy' },
-    { name: 'बिचार', link: '/opinion' },
-    { name: 'नेपाली ब्राण्ड', link: '/nepali-brand' },
-    { name: 'समाज', link: '/society' },
-    { name: 'कला', link: '/arts' },
-    { name: 'ब्लग', link: '/blog' },
-    { name: 'खेलकुद', link: '/sports' },
-    { name: 'ग्लोबल', link: '/global' },
-    { name: 'कमर स्टोरी', link: '/kamar-story' },
-  ];
-
-  toggleSearch() {
-    this.showSearch = !this.showSearch;
-    if (!this.showSearch) {
-      this.searchQuery = '';
-    }
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
   }
 
-  onSearchSubmit() {
-    if (this.searchQuery.trim()) {
-      console.log('Searching for:', this.searchQuery);
-    }
-  }
-
-  onSubscribe() {
-    console.log('Subscribe clicked');
-  }
-
-  onLogin() {
-    console.log('Login clicked');
+  closeDropdown(): void {
+    this.isDropdownOpen = false;
   }
 }
