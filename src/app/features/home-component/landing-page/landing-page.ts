2257c831b51f846e0c5702f0a122887b756cdf4e
aayushskp <krishnapanthi94@gmail.com>
2025-11-18 11:08:59 +0545

    addded the hero section to the landing pages with adjustment to navbar

--- src/app/features/home-component/landing-page/landing-page.ts
+++ src/app/features/home-component/landing-page/landing-page.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
 export class LandingPage {
-
+  stars: any[] = [];
+  ngOnInit() {
+    for (let i = 0; i < 150; i++) {
+      this.stars.push({
+        size: Math.random() * 2.5 + 0.5,
+        top: Math.random() * 70,
+        left: Math.random() * 100,
+        opacity: Math.random() * 0.7 + 0.3,
+        duration: Math.random() * 3 + 2,
+        delay: Math.random() * 2,
+      });
+    }
+  }
 }
